3c8953f81303f05f781810bcf5e4b606bc79ac70
Mirian Kekeocha <miriankekeocha@hotmail.com>
2022-11-15 19:15:29 -0600

    first scan

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,22 +1,18 @@
 package com.example;
-
 /**
  * This is a class.
  */
 public class Greeter {
-
   /**
    * This is a constructor.
    */
   public Greeter() {
-
   }
-
  /**
    * This is a method.
    */
   public final String greet(final String someone) {
-    String DB_PASSWORD = 'Admin@12345'
-    return String.format("Hello Mimi, %s!", someone);
+    String DB_PASSWORD = "Admin@12345";
+    return String.format("Hello Avinash, %s!", someone);
   }
 }
